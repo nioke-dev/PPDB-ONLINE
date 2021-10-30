b65ddb28f3d3f3a3902e533030cefa639d10fffc
Nurul Mustofa <66587167+nioke-dev@users.noreply.github.com>
2021-10-30 20:28:43 +0700

    desain web part 3 rampung

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
     <!-- link css -->
-    <link rel="stylesheet" href="stylee.css">
+    <link rel="stylesheet" href="style.css">
 
     <!-- link fonts poppins all family -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -78,7 +78,7 @@
 
     <section>
         <div class="container">
-            <div class="text-center">
+            <div class="text-center mb-5">
                 <h3>KENAPA PILIH <br> SMKN 1 GENDING ?</h3>
             </div>
             <div class="row">
@@ -152,6 +152,65 @@
         </div>
     </section>
 
+    <section class="tentang">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="Assets/img/ilustration/undraw_all_the_data_re_hh4w.svg" alt="tentang sekolah" class="img-fluid mx-auto d-block">
+                </div>
+                <div class="col-md-6">
+                    <h3 class="font-weight-bold mb-3 mt-5 ">TENTANG SMKN 1 GENDING</h3>
+                    <p class="deskripsi">
+                        SMKN 1 GENDING Adalah Sekolah menengah kejuruan negeri yang berdiri sejak 2003. SMKN 1GENDING Adalah Sekolah menengah kejuruan negeri yang berdiri sejak 2003. SMKNDU Adalah Sekolah menengah kejuruan negeri yang berdiri sejak 2003. SMKNDU Adalah Sekolah menengah kejuruan negeri yang berdiri sejak 2003.
+                    </p>
+
+                    <p class="font-weight-bold mt-4">Kontak Kami</p>
+                    <ul class="social">
+                        <li>
+                            <a href="tel:085161644408">
+                                <img src="Assets/img/icon/phone-symbol-of-an-auricular-inside-a-circle_icon-icons.com_56478.png" alt="telephone">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="mailto:nioke8090@gmail.com">
+                                <img src="Assets/img/icon/mail_email_message_electronic_online_web_icon-icons.com_59986.png" alt="email">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/groups/709516699779630">
+                                <img src="Assets/img/icon/social_facebook_fb_35.png" alt="facebook">
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.smkn1gending.sch.id/">
+                                <img src="Assets/img/icon/seo-social-web-network-internet_174_icon-icons.com_61537.png" alt="website">
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="bg-primary pt-5 pb-5">
+        <div class="container">
+            <p class="text-white text-center">STATISTIK PENDAFTAR GELOMBANG 2</p>
+            <div class="row">
+                <div class="col-md-4 text-center">
+                    <h3 class="text-white font-weight-bold h1">293</h3>
+                    <p class="text-white">TOTAL PENDAFTAR</p>
+                </div>
+                <div class="col-md-4 text-center">
+                    <h3 class="text-white font-weight-bold h1">293</h3>
+                    <p class="text-white">LAKI LAKI</p>
+                </div>
+                <div class="col-md-4 text-center">
+                    <h3 class="text-white font-weight-bold h1">293</h3>
+                    <p class="text-white">PEREMPUAN</p>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
     <!-- Optional JavaScript; choose one of the two! -->
